1b532908d5ad2fd6146d00066bfbcc5bdec3a844
Sujal Lugare <lugaresujal@gmail.com>
2026-04-09 15:28:32 +0530

    first commit

---
+++ matplotlib/piechart.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.pie([10,20,30,40],labels=['A','B','c','D'])
+
+plt.savefig('plot.png')
+plt.show()
---
+++ matplotlib/visualization.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y= [2,3,5,7,11]
+# plt.plot(x,y)
+# plt.show()     # line Chart
+
+# plt.bar(x,y)
+# plt.show()   # bar chart
+
+# data = [10,20,20,20,30,30,30,40,50]
+# plt.hist(data,bins=5)
+# plt.show()
